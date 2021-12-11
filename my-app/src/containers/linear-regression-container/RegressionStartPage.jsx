9a78791cc3690f37d44331d0445306bdc4e4aa4b
Barbara <bb49992@fer.hr>
2021-12-11 18:31:31 +0100

    Add wizard structure for Regression page

--- my-app/src/containers/linear-regression-container/RegressionStartPage.jsx
+++ my-app/src/containers/linear-regression-container/RegressionStartPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-
+import StepWizard from 'react-step-wizard'
+import Step1 from './Step1'
+import Step2 from './Step2'
+import Step3 from './Step3'
 class RegressionStartPage extends React.Component {
   constructor(props) {
     super(props)
@@ -9,8 +12,13 @@ class RegressionStartPage extends React.Component {
 
   render() {
     return (
-      <Container className='justify-content-center' style={{ textAlign:"center",  width: '80em', height: '40em', background: 'rgb(128, 204, 255, 0.3)'}}>
-        <h1>Regression start page</h1>
+      <Container className='justify-content-center' style={{ textAlign:"center", minHeight: '40em', background: 'rgb(197, 235, 202, 0.5)'}}>
+        <h1>Linear Regression</h1>
+        <StepWizard>
+          <Step1/>
+          <Step2></Step2>
+          <Step3></Step3>
+        </StepWizard>
       </Container>
     )
   }
---
+++ my-app/src/containers/linear-regression-container/Step2.jsx
@@ -0,0 +1,24 @@
+import { Container, Row, Button, Col } from "react-bootstrap";
+
+const Step2 = (props) => {
+
+
+    return (
+        <Container className='justify-content-center' style={{ textAlign: "center", width: '80em', background: 'rgb(252, 249, 242)', paddingBottom: "1em" }}>
+        <Container style={{ textAlign: "center", height: '30em'}}>
+            <h1>Step2</h1>
+        </Container>
+        <Row>
+            <Col >
+                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.firstStep}>First Step</Button>
+                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.previousStep}>Previous Step</Button>
+                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} disabled>Current Step:{props.currentStep} </Button>
+                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.nextStep}>Next Step</Button>
+                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={() => props.goToStep(3)}>Last Step</Button>
+            </Col>
+        </Row>
+    </Container>
+    );
+}
+
+export default Step2;
