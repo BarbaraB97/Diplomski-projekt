6449345ae88a04568a7ac192ed326852c5af125c
Jelena Marković <jelena.markovic@fer.hr>
2021-12-15 22:58:51 +0100

    added table and description for Step 2

---
+++ my-app/src/components/Simple_lin_reg_dataset.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Table } from 'react-bootstrap';
+
+const Dataset = ({ areFeaturesHighlighted, isLabelHighlighted, chosenFeature }) => {
+
+
+	const housePricesDataset = [
+        { price: 100, size: 10 },
+        { price: 200, size: 25 },
+		{ price: 500, size: 100 },
+		{ price: 1000, size: 220 },
+		{ price: 2000, size: 400 }
+	]
+
+    const generateTable = () => {
+        return housePricesDataset.map((record, index) => {
+            const { price,size } = record;
+            return (
+                <tr key={index}>
+                    <td>{++index}</td>
+                    <td style={{backgroundColor: chosenFeature === "Price" ? "rgb(197, 235, 202)" : null }}>{price}</td>
+                    <td style={{backgroundColor: chosenFeature === "Size" ? "rgb(197, 235, 202)" : null }}>{size}</td>
+                </tr>
+            )
+        })
+    }
+    
+
+
+
+    return (
+        <Table striped bordered hover id="weatherTable" style={{ heigth: "35em", width: "30em", backgroundColor: "rgb(197, 235, 202, 0.3)" }}>
+            <thead>
+                <tr id="features">
+                    <th>#</th>
+                    <th style={{ backgroundColor: isLabelHighlighted ? null : areFeaturesHighlighted ? "rgb(197, 235, 202)" : null }}>Price (in thousands $)</th>
+                    <th style={{ backgroundColor: isLabelHighlighted ? null : areFeaturesHighlighted ? "rgb(197, 235, 202)" : null }}>Size (Square meters)</th>
+                </tr>
+            </thead>
+            <tbody>
+                {generateTable()}
+            </tbody>
+        </Table>
+    )
+}
+
+export default Dataset;
--- my-app/src/containers/linear-regression-container/Step2.jsx
+++ my-app/src/containers/linear-regression-container/Step2.jsx
@@ -1,23 +1,48 @@
+import { useState } from "react";
 import { Container, Row, Button, Col } from "react-bootstrap";
+import Dataset from '../../components/Simple_lin_reg_dataset'
 
 const Step2 = (props) => {
 
+    const [areFeaturesHighlighted, setHighlightFeatures] = useState(false);
+    const [isLabelHighlighted, setHighlightLabel] = useState(false);
+
+    const highlightFeatures = () => {
+        setHighlightFeatures(!areFeaturesHighlighted);
+    }
+
+    const hightlightLabel = () => {
+        setHighlightLabel(!isLabelHighlighted);
+    }
 
     return (
-        <Container className='justify-content-center' style={{ textAlign: "center", width: '80em', background: 'rgb(252, 249, 242)', paddingBottom: "1em" }}>
-        <Container style={{ textAlign: "center", height: '30em'}}>
-            <h1>Step2</h1>
+        <Container style={{ width: '80em', background: 'rgb(252, 249, 242)', paddingBottom: "1em" }}>
+            <Container style={{ paddingTop: "2em", paddingBottom: "2em" }}>
+                <h3>Simple linear regression</h3>
+                <hr></hr><br />
+  
+                <Row>
+                    <Col>
+                        <Dataset areFeaturesHighlighted={areFeaturesHighlighted} isLabelHighlighted={isLabelHighlighted}></Dataset>
+                    </Col>
+                    <Col style={{ textAlign: "left" }}>
+                    <b>Simple linear regression</b> is a linear regression model with a single explanatory variable. That is, it concerns two-dimensional sample points 
+                    with one independent and one dependent variable and finds a linear function that, 
+                    as accurately as possible, predicts the dependent variable values as a function of the independent variable.<br /><br />
+                    </Col>
+                </Row>
+
+            </Container>
+            <Row>
+                <Col >
+                    <Button style={{ backgroundColor: "rgb(197, 235, 202)", color: "rgb(0,0,0)", borderColor: "rgb(158, 250, 192)" }} onClick={props.firstStep}>First Step</Button>
+                    <Button style={{ backgroundColor: "rgb(197, 235, 202)", color: "rgb(0,0,0)", borderColor: "rgb(158, 250, 192)" }} onClick={props.previousStep}>Previous Step</Button>
+                    <Button style={{ backgroundColor: "rgb(197, 235, 202)", color: "rgb(0,0,0)", borderColor: "rgb(158, 250, 192)" }} disabled>Current Step:{props.currentStep} </Button>
+                    <Button style={{ backgroundColor: "rgb(197, 235, 202)", color: "rgb(0,0,0)", borderColor: "rgb(158, 250, 192)" }} onClick={props.nextStep}>Next Step</Button>
+                    <Button style={{ backgroundColor: "rgb(197, 235, 202)", color: "rgb(0,0,0)", borderColor: "rgb(158, 250, 192)" }} onClick={() => props.goToStep(5)}>Last Step</Button>
+                </Col>
+            </Row>
         </Container>
-        <Row>
-            <Col >
-                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.firstStep}>First Step</Button>
-                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.previousStep}>Previous Step</Button>
-                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} disabled>Current Step:{props.currentStep} </Button>
-                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={props.nextStep}>Next Step</Button>
-                <Button style={{backgroundColor:"rgb(197, 235, 202)", color:"rgb(0,0,0)", borderColor:"rgb(158, 250, 192)"}} onClick={() => props.goToStep(3)}>Last Step</Button>
-            </Col>
-        </Row>
-    </Container>
     );
 }
 
